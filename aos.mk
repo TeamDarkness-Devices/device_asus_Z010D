c798d7e99cf87e22e206e6c58363f2d35ad674f3
Ninad Patil <ninadpatil100@gmail.com>
2017-07-24 22:07:04 +0530

    Atomic-Os BringUp

--- aos.mk
+++ aos.mk
@@ -1,10 +1,10 @@
 # Inherit from device specific
 $(call inherit-product, device/asus/Z010D/full_Z010D.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit Atomic OS custom product configuration.
+$(call inherit-product, vendor/aos/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_Z010D
+PRODUCT_NAME := aos_Z010D
 PRODUCT_DEVICE := Z010D
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo aos_Z010D-userdebug
